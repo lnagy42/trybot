70aae214f7c0fb8c4d7c2b439a47fa0fd9c2a636
lnagy42 <lnagy42@student.42.fr>
2017-08-14 16:32:31 +0200

    gitignore

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-var apiai = require(‘apiai’);
+var apiai = require('apiai');
 
 var app = apiai(“98ba98c2c62049b0a3600dda8512ac89“);
 
-var request = app.textRequest(‘Bonjour Olympe, bienvenue au monde‘, {
-    sessionId: ‘Some unique sessionId’
+var request = app.textRequest('Bonjour Olympe, bienvenue au monde', {
+    sessionId: 'Some unique sessionId'
 });
 
-request.on(‘response’, function(response) {
+request.on('response, function(response) {
     console.log(response);
 });
 
-request.on(‘error’, function(error) {
+request.on('error', function(error) {
     console.log(error);
 });
 
